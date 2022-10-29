587c2ae2804e4950ff8fa71d7b1ea1ae712722c8
vladyslavkosenko <vaduha91@gmail.com>
2022-10-29 21:51:32 +0300

    add assertj

--- src/main/java/com/example/start/task2/dto/User.java
+++ src/main/java/com/example/start/task2/dto/User.java
@@ -2,7 +2,7 @@ package com.example.start.task2.dto;
 
 import lombok.Value;
 
-//@Value(staticConstructor = "of")
+@Value(staticConstructor = "of")
 public class User {
     Integer id;
     String username;
--- src/test/java/com/example/start/task2/service/UserServiceTest.java
+++ src/test/java/com/example/start/task2/service/UserServiceTest.java
@@ -3,16 +3,19 @@ package com.example.start.task2.service;
 import com.example.start.task2.dto.User;
 import org.junit.jupiter.api.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User PETR = User.of(2, "Petr", "111");
 
     private UserService userService;
 
     @BeforeAll
-     void init() {
+    void init() {
         System.out.println("BeforeAll ");
     }
 
@@ -34,10 +37,39 @@ public class UserServiceTest {
     @Test
     void userSizeIfUserAdded() {
         System.out.println("test 2: " + this);
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(IVAN);
+        userService.add(PETR);
         var users = userService.getAll();
-        assertEquals(2, users.size());
+        assertThat(users).hasSize(2);
+        //   assertEquals(2, users.size());
+
+    }
+
+    @Test
+    void loginSuccessIfUserExist() {
+        userService.add(IVAN);
+        var maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+        assertThat(maybeUser).isPresent();
+        //assertTrue(maybeUser.isPresent());
+        // maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+        maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
+        System.out.println(maybeUser + " is empty?");
+    }
+
+    @Test
+    void loginFailIIfPasswordIsNotCorrect() {
+        userService.add(IVAN);
+        var maybeUser = userService.login(IVAN.getUsername(), "dummy");
+        assertTrue(maybeUser.isEmpty());
+
+    }
+
+    @Test
+    void loginFailIIfUserDoesExist() {
+        userService.add(IVAN);
+        var maybeUser = userService.login("dummy", IVAN.getPassword());
+        assertTrue(maybeUser.isEmpty());
+        System.out.println(maybeUser + " is empty?");
 
     }
 
